27e28c337601da980d45d8da5d2793c605639afa
RalatPC <ralatrado@gmail.com>
2022-10-01 14:48:59 +0100

    added the mouse hold and move to draw

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
 const container = document.querySelector('.container');
 
 for (let i = 1; i <= 256; i++) {
-    container.innerHTML += `<div class="box box${i}"></div>`;
+    container.innerHTML += `<div class="box${i}"></div>`;
+}
+
+const sketchBoxes = document.querySelectorAll('.container > *');
+
+
+sketchBoxes.forEach(box => box.addEventListener('mouseover', changeColor));
+sketchBoxes.forEach(box => box.addEventListener('mousedown', changeColor));
+// sketchBoxes.forEach(box => box.addEventListener('mouseo', changeColor));
+
+
+let mouseDown = false;
+
+container.addEventListener('mousedown', (e) => {
+    if (e.type === 'mousedown') {
+        mouseDown = true;
+        console.log(mouseDown);
+    }
+});
+
+container.addEventListener('mouseup', (e) => {
+    if (e.type === 'mouseup') {
+        mouseDown = false;
+        console.log(mouseDown);
+    }
+});
+
+function changeColor() {
+    if (mouseDown) {
+        this.classList.add('clicked');
+    }
 }
