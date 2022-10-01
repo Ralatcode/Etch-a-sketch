370f75e48d952f44e492805cf8fe410ce1d1acb2
RalatPC <ralatrado@gmail.com>
2022-10-01 15:08:57 +0100

    created function for container mouse up and down

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const container = document.querySelector('.container');
 
+const gridsize = '';
+
 for (let i = 1; i <= 256; i++) {
     container.innerHTML += `<div class="box${i}"></div>`;
 }
@@ -9,27 +11,31 @@ const sketchBoxes = document.querySelectorAll('.container > *');
 
 sketchBoxes.forEach(box => box.addEventListener('mouseover', changeColor));
 sketchBoxes.forEach(box => box.addEventListener('mousedown', changeColor));
-// sketchBoxes.forEach(box => box.addEventListener('mouseo', changeColor));
 
 
 let mouseDown = false;
 
-container.addEventListener('mousedown', (e) => {
-    if (e.type === 'mousedown') {
-        mouseDown = true;
-        console.log(mouseDown);
-    }
-});
+// mousedown event
+container.addEventListener('mousedown', checkMouse);
+// mouseup event
+container.addEventListener('mouseup', checkMouse);
 
-container.addEventListener('mouseup', (e) => {
-    if (e.type === 'mouseup') {
-        mouseDown = false;
-        console.log(mouseDown);
-    }
-});
 
+// adds clicked class to style hovered box
 function changeColor() {
     if (mouseDown) {
         this.classList.add('clicked');
+    }
+}
+// checks if the mouse is held down on the container 
+// and assigns true to the mouseDown variable
+
+// checks if the mouse is not held down on the container and 
+// assigns false to the mouseDown variable
+function checkMouse(e) {
+    if (e.type === 'mousedown') {
+        mouseDown = true;
+    } else if (e.type === 'mouseup') {
+        mouseDown = false;
     }
 }
